4e1e37eff7e6a53132672333b1ff76b7080c8ade
TempestAethel <shreeneethreddy@gmail.com>
2025-06-05 13:38:55 +0530

    .v2

--- Files/MAX/vite.config.js
+++ Files/MAX/vite.config.js
@@ -1,10 +1,9 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
-// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: './', // Add this line to make assets load correctly on GitHub Pages
+  base: './',
   optimizeDeps: {
     exclude: ['lucide-react'],
   },
